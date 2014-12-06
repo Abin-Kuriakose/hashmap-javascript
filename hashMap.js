63d351614b4c09bb65d5702a7ee424f761ea6929
Abin-Kuriakose <euroabin@gmail.com>
2014-12-07 03:23:50 +0530

    Update hashMap.js

--- hashMap.js
+++ hashMap.js
@@ -1,12 +1,12 @@
 var hashMap=function(){
-    this.hashArray={};//dictionary
+    this.hashDict={};//dictionary
     this.size=0;
     this.debug=true;
     return this;
 }
 hashMap.prototype.add=function(_key,_value){
-    if (!this.hashArray.hasOwnProperty(_key)) {
-        this.hashArray[_key] = _value;
+    if (!this.hashDict.hasOwnProperty(_key)) {
+        this.hashDict[_key] = _value;
         ++this.size;
     }
     else if(this.debug)
@@ -14,7 +14,6 @@ hashMap.prototype.add=function(_key,_value){
     
 }
 hashMap.prototype.addAll=function(_keyArray,_valueArray){
-    //note:addAll
     if(_keyArray.length===_valueArray.length) {
         _l=_keyArray.length;
         for(i=0;i<_l;i++) {
@@ -25,17 +24,17 @@ hashMap.prototype.addAll=function(_keyArray,_valueArray){
     }
 }
 hashMap.prototype.remove=function(_key){
-    if (this.hashArray.hasOwnProperty(_key)) {
-        delete this.hashArray[_key];
+    if (this.hashDict.hasOwnProperty(_key)) {
+        delete this.hashDict[_key];
         --this.size;
     }
     else if(this.debug)
         throw 'key not found';
 }
 hashMap.prototype.get=function(_key){
-    if (this.hashArray.hasOwnProperty(_key))
+    if (this.hashDict.hasOwnProperty(_key))
     {
-        return this.hashArray[_key];
+        return this.hashDict[_key];
     }
     else if(this.debug)
         throw 'no key found';
